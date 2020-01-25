398f6b8621e0778f49b9404741066caef273a659
lucasdesousa19@hotmail.com <informaticact@santoagostinho.com.br>
2020-01-24 23:59:03 -0300

    add all

---
+++ src/routes.js
@@ -0,0 +1,33 @@
+import { createAppContainer } from 'react-navigation'
+import { crateStackNavigator, createStackNavigator } from 'react-navigation-stack'
+
+import Main from './pages/Main'
+import Profile from './pages/Profile'
+
+const Routes = createAppContainer(
+  createStackNavigator({
+    Main:{
+      screen: Main,
+      navigationOptions:{
+        title: 'DevRadar',
+      }
+    },
+    Profile:{
+      screen: Profile,
+      navigationOptions: {
+        title: 'Perfil no Github'
+      }
+    }
+  }, {
+    defaultNavigationOptions: {
+      headerTintColor:'#fff',
+      headerTitleAlign: 'center',
+      headerStyle:{
+        backgroundColor: '#6b52ae',
+        
+      }
+    }
+  })
+)
+
+export default Routes
